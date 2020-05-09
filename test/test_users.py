dc3a86845980bd7eceab650935209c430e4e1a19
Sachin Magar <smagar77@gmail.com>
2020-05-09 11:32:54 +0530

    test_users.py

--- test/test_users.py
+++ test/test_users.py
@@ -16,7 +16,7 @@ class test_user(BaseTest):
   @patch('flask_jwt_extended.view_decorators.verify_jwt_in_request')
   def test_post(self, *args):
     result = self.client.post('http://localhost:5000/users/add_user/', data=json.dumps({"":""}), headers={"Content-Type":"application/json", "Authorization":"Bearer {0}"})
-    #self.assertEqual(result.status_code, 200)
+    self.assertEqual(result.status_code, 200)
 
   @patch('flask_jwt_extended.view_decorators.verify_jwt_in_request')
   @patch('api.services.Users.User.delete_uesr')
